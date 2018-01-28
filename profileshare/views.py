52955b410373c724d4da1008ed3f3b757d3de0dc
pavankm <kpavan@protonmail.com>
2018-01-27 18:57:40 -0600

    flask QR extention

--- profileshare/views.py
+++ profileshare/views.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file, request
+from flask_qrcode import QRcode
 
 app = Flask(__name__)
+qrcode = QRcode(app)
 
 
 @app.route('/')
@@ -11,3 +13,17 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+@app.route('/qrcode', methods=['GET'])
+def get_qrcode():
+    # please get /qrcode?data=<qrcode_data>
+    data = request.args.get('data', '')
+    return send_file(
+        qrcode(data, mode='raw'),
+        mimetype='image/png'
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
