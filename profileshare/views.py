88a35883ad375a9160c66861050965c15a8dfa59
pavankm <kpavan@protonmail.com>
2018-02-10 15:51:00 -0600

    uncommon hacks initial commit

--- profileshare/views.py
+++ profileshare/views.py
@@ -1,7 +1,18 @@
-from flask import Flask, send_file, request, render_template, url_for
+from flask import Flask, send_file, request, render_template, url_for, redirect, flash
 from flask_qrcode import QRcode
+import os
+from werkzeug.utils import secure_filename
+
+# for decode
+from pyzbar.pyzbar import decode, ZBarSymbol
+from PIL import Image
+
+# TODO
+UPLOAD_FOLDER = '/home/pavan/profileshare'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 qrcode = QRcode(app)
 
 
@@ -23,6 +34,33 @@ def get_qrcode():
         qrcode(data, mode='raw'),
         mimetype='image/png'
     )
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            data = decode(Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename)), symbols=[ZBarSymbol.QRCODE])
+            # TODO lookup in DB & render profile page
+            # request.form['username'] to extract
+            profile = data[0].data
+            return render_template('response.html', fb="pavankm", ig="yo", user=profile, uniqID=profile)
+    return '', 204
 
 
 if __name__ == '__main__':
